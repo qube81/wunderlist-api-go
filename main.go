1cdaf672d8394afde50306a1862dd303e04b0e7b
qube81 <yuta.tkhs@gmail.com>
2015-11-22 21:44:18 +0900

    modify client output

--- main.go
+++ main.go
@@ -14,15 +14,25 @@ func main() {
 	client := wunderlist.NewClient(clientID, clientSecret)
 
 	lists, _ := client.List.GetAll()
-	list, _ := client.List.Get(lists[1].ID)
-	user, _ := client.User.Get()
-	tasks, _ := client.Task.GetByListID(lists[1].ID, false)
-	task, _ := client.Task.Get(tasks[0].ID)
+	for _, list := range lists {
+		tasks, _ := client.Task.GetByListID(list.ID)
+		pp.Println("===" + list.Title + "===")
+		for _, task := range tasks {
+			pp.Println(task.Title, task.DueDate)
+		}
+	}
 
-	pp.Print(lists)
-	pp.Print(list)
-	pp.Print(user)
-	pp.Print(tasks)
-	pp.Print(task)
+	/*
+		list, _ := client.List.Get(lists[1].ID)
+		user, _ := client.User.Get()
+		tasks, _ := client.Task.GetByListID(lists[1].ID, false)
+		task, _ := client.Task.Get(tasks[0].ID)
+
+		pp.Print(lists)
+		pp.Print(list)
+		pp.Print(user)
+		pp.Print(tasks)
+		pp.Print(task)
+	*/
 
 }
