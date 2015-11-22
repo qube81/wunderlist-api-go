ca29288b6e7bb321c7d1dc99b57ac738b2d55a71
qube81 <yuta.tkhs@gmail.com>
2015-11-22 15:56:59 +0900

    add http.Client

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"github.com/k0kubun/pp"
 	"github.com/qube81/wunderlist-api-go/wunderlist"
+	"os"
 )
 
 func main() {
-	wunderlist.Hello()
+
+	clientID := os.Getenv("WL_CLIENT_ID")
+	clientSecret := os.Getenv("WL_ACCESS_TOKEN")
+
+	client := wunderlist.NewClient(clientID, clientSecret)
+
+	pp.Print(client.Get("lists"))
 }
--- wunderlist/wunderlist.go
+++ wunderlist/wunderlist.go
@@ -1,9 +1,64 @@
 package wunderlist
 
 import (
-	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
 )
 
-func Hello() {
-	fmt.Print("Hello, wunderlist!")
+const (
+	endpoint = "https://a.wunderlist.com/api/v1/"
+)
+
+var httpClient = &http.Client{
+	Timeout: time.Duration(5) * time.Second,
+}
+
+// Client for wunderlist API
+type Client struct {
+	clientID    string
+	accessToken string
+	httpClient  *http.Client
+}
+
+// NewClient generate and return Client
+func NewClient(clientID string, accessToken string) *Client {
+
+	c := &Client{
+		clientID:    clientID,
+		accessToken: accessToken,
+		httpClient:  httpClient,
+	}
+
+	return c
+}
+
+// Get request HTTP GET via Client
+func (c *Client) Get(path string) (body string, err error) {
+
+	req, err := http.NewRequest("GET", endpoint+path, nil)
+	if err != nil {
+		return "", err
+	}
+	return Execute(c, req)
+}
+
+// Execute HTTP request
+func Execute(c *Client, req *http.Request) (body string, err error) {
+
+	req.Header.Add("X-Client-ID", c.clientID)
+	req.Header.Add("X-Access-Token", c.accessToken)
+
+	resp, err := c.httpClient.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(respBody), err
 }
