1d596dc0c81565abcf3f2546a389f870ea3df061
qube81 <yuta.tkhs@gmail.com>
2015-11-22 14:24:19 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/qube81/wunderlist-api-go/wunderlist"
+)
+
+func main() {
+	wunderlist.Hello()
+}
---
+++ wunderlist/wunderlist.go
@@ -0,0 +1,9 @@
+package wunderlist
+
+import (
+	"fmt"
+)
+
+func Hello() {
+	fmt.Print("Hello, wunderlist!")
+}
